1e3fcab2a84cade5d6bf54ff2564c9e5cf71885b
WEI-CHUN KAO <weichun@omniscientai.com>
2023-01-17 11:39:18 +0800

    use re_path instead of url

--- rest_captcha/urls.py
+++ rest_captcha/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
+from django.urls import re_path
 from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.RestCaptchaView.as_view(), name='rest_captcha'),
+    re_path(r'^$', views.RestCaptchaView.as_view(), name='rest_captcha'),
 ]
